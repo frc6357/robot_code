66131d88853168d274626352143dd5c3370678b4
Jeffrey Erickson <jee7s@alumni.virginia.edu>
2017-05-09 23:05:20 -0500

    adding basic test that always passes

---
+++ SpringKonstant/test/org/usfirst/frc6357/SpringKonstant/tests/AlwaysPassesTest.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team246.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlwaysPassesTest {
+
+	@Test
+	public void test() {
+		assertTrue("Nothing to test right now", true);
+	}
+
+}
